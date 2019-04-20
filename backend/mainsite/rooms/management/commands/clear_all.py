3be8343b8f1dc0829554bfcd266b9c2b1bfe17bd
KiranPesarlanka <kiran@deepcompute.com>
2019-04-20 17:31:58 +0530

    Update clear_all.py

--- backend/mainsite/rooms/management/commands/clear_all.py
+++ backend/mainsite/rooms/management/commands/clear_all.py
@@ -3,7 +3,7 @@ from rooms.models import NormalRoom, DeluxeRoom
 
 
 class Command(BaseCommand):
-    help = 'This is for creating rooms'
+    help = 'This is for clearing rooms'
 
     def add_arguments(self, parser):
         parser.add_argument('no_of_rooms', type=int)
